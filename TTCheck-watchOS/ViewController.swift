765e97cc2ba63bd920c6af3f72ac76dd86441826
jp <juan.ghelfi@tictapps.com>
2017-11-28 20:33:04 -0300

    Initial Commit

---
+++ TTCheck-watchOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TTCheck-watchOS
+//
+//  Created by Juan Pablo on 11/28/17.
+//  Copyright © 2017 Juan Pablo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
